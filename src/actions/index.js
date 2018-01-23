c2ac242f371540b65fb1275e758e203088eb2bd3
Edgar Dewsbery <edewsbery@gmail.com>
2018-01-23 18:32:24 +0000

    validate and handlesubmit

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,9 +3,10 @@ import { connect } from 'react-redux';
 import {fetchposts} from '../actions';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=evens2468'
+const API_KEY = '?key=evens2468';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
@@ -14,4 +15,15 @@ export function fetchPosts() {
     type: FETCH_POSTS,
     payload: request
   };
+}
+
+export const createPost = (values, redirectCallback) => {
+  console.log('action received', values);
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+  .then(() => redirectCallback());
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
 }
